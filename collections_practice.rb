35a0c3e93f3e374eab8766e4a93afe402c926b63
Kate Summerill <kaitlynsummerill@gmail.com>
2019-12-04 15:05:43 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,20 @@
+# returns a copy of the array with the integers in ascending order.
 
+def sort_array_asc(integers)
+  integers.sort
+end
+
+# returns a copy of the array with the integers in descending order.
+def sort_array_desc(integers)
+  integers.sort.reverse
+end
+
+# returns a copy of the array with strings orders in ascending order by length
+def sort_array_char_count(array)
+  array.sort_by(&:length)
+end
+
+# swaps the second and third elements of the array
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+end
